b466d314d1e833a1529130b06570a3ca751cc5a3
Chris Kite <chris@chriskite.com>
2014-06-02 15:59:59 -0500

    doc runs api

--- server/app/controllers/api/v1/runs_controller.rb
+++ server/app/controllers/api/v1/runs_controller.rb
@@ -1,6 +1,39 @@
 module Api
   module V1
     class RunsController < ApplicationController
+      
+      #
+      # GET /runs
+      # Return all in-progress runs
+      #
+      def index
+      
+      end
+
+      #
+      # GET /runs/:id
+      # Return the single run specified by +id+
+      #
+      def show
+
+      end
+
+      #
+      # POST /runs
+      # Create a new run with the uploaded source code
+      # Return the created run
+      #
+      def create
+
+      end
+
+      #
+      # DELETE /runs/:id
+      # Attempt to kill and then delete run specified by +id+
+      #
+      def destroy
+
+      end
 
     end
   end
