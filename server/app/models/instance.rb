02f35b9db97cfdc092d7ccabeb578a8822840933
Ryan Zabcik <ryanzabcik@gmail.com>
2014-07-18 15:21:40 -0500

    abstractions for connection, which fail automatically

--- server/app/models/instance.rb
+++ server/app/models/instance.rb
@@ -2,25 +2,50 @@ require 'fog'
 require 'rest-client'
 
 DIR = '/home/yarddog/workspace'
-PORT = 60086
+AGENT_PORT  = 60086
+DOCKER_PORT = 60186
 
 # extending this class for yarddog
 class Fog::Compute::AWS::Server
 
-    def url req
-        "#{private_ip_address}:#{PORT}/#{req}"
-    end
-
     def connected?
-        ready? && private_ip_address &&
-            (RestClient.get (url 'test') rescue false)
+        agent['test'].get rescue false
     end
 
     def upload_image file
-        fail 'Not connected.' unless connected?
-        # high timeout for debugging purposes
-        res = RestClient::Resource.new("#{private_ip_address}:#{PORT}/", timeout: 999999)
-        res['images'].post file: File.new(file)
+        agent['images'].post file: File.new(file)
+    end
+
+    def kill container, image
+        docker["containers/#{container}"].delete
+        docker["images/#{image}"].delete
+    end
+
+    private
+    # abstractions for connection, which fail automatically
+
+    def agent
+        return @agent if @agent
+        if ready? && private_ip_address
+            @agent = RestClient::Resource.new(
+                "#{private_ip_address}:#{AGENT_PORT}/",
+                timeout: 999999
+            )
+        else
+            fail 'Server is not ready.'
+        end
+    end
+
+    def docker
+        return @docker if @docker
+        if ready? && private_ip_address
+            @docker = RestClient::Resource.new(
+                "#{private_ip_address}:#{DOCKER_PORT}/",
+                timeout: 999999
+            )
+        else
+            fail 'Server is not ready.'
+        end
     end
 
 end
