35d926bcacc7ea25bd567bf8cb78dfd470b99323
Ryan Zabcik <ryanzabcik@gmail.com>
2014-07-30 13:19:49 -0500

    fix DOCKER_OPTS variable; needed proto prefixes

--- ami-install.sh
+++ ami-install.sh
@@ -11,4 +11,4 @@ curl -sSL https://get.docker.io/gpg | apt-key add -
 echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install lxc-docker
-echo 'DOCKER_OPTS="-H 0.0.0.0:60186 -H /var/run/docker.sock' >> /etc/default/docker
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:60186 -H unix:///var/run/docker.sock"' >> /etc/default/docker
